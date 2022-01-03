553f33466c440e40e964b3f4cce6e70cd7e616af
JC-ORBEZO <jose.orbezo@alumnos.frgp.utn.edu.ar>
2022-01-03 11:43:35 -0300

    Cree las carpetas Header, Main y Footer como estructura inicial/principal de la pag web.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,45 @@
+const Header = () => {
+    return ( 
+        <>
+            <img src="https://sieleckimingo.com/wp-content/uploads/2020/10/logo-3p.png" alt="Logo" width={550} height={100}/>
+            {/*<div>
+                <div>
+                    <i class="fas fa-ambulance"></i>
+                    <p>Servicio de Alquiles de Ambulancias en Buenos Aires</p>
+                    <hr/>
+                    <p>Ofrecemos servicios para eventos sociales y deportivos.</p>
+                    <hr/>
+                    <p>Traslados a centro de vacunación COVID</p>
+                </div>                
+                <div>
+                    <form>
+                    <label>
+                        *Nombre y Apellido:
+                        <input type="text" name="name" />
+                    </label>
+                    <label>
+                        *Telefono:
+                        <input type="text" name="name" />
+                    </label>
+                    <label>
+                        *Email:
+                        <input type="text" name="name" />
+                    </label>
+                    <label>
+                        *Ciudad:
+                        <input type="text" name="name" />
+                    </label>
+                    <label>
+                        *Mensaje:
+                        <textarea></textarea>
+                    </label>
+                    <input type={"submit"}/>
+                   
+                    </form>
+                </div>                
+            </div> */}
+        </>
+     );
+}
+ 
+export default Header;
